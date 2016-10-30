b6f410ab62bdd2520685653ed42d4ec8db90e6ee
Josh <jalexy12@gmail.com>
2016-10-30 12:22:19 -0400

    End of redux todo

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,42 +1,9 @@
 import { combineReducers } from 'redux'
-import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions/actions'
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from '../actions/actions'
 const { SHOW_ALL } = VisibilityFilters
 
-const initialState = {
-  visibiltyFilter: VisibilityFilters.SHOW_ALL,
-  todos: []
-}
-
-function todos(state = [], action){
-  switch (action.type){
-    case ADD_TODO:
-      return Object.assign({}, state, {
-        todos: [
-          ...state.todos,
-          {
-            text: action.text,
-            completed: false
-          }
-        ]
-      })
-      case TOGGLE_TODO
-        return Object.assign({}, state, {
-          todos: state.todos.map((todo, index) => {
-            if (index === action.index){
-              return Object.assign({}, todo, {
-                completed: !todo.completed
-              })
-            }
-            return todo
-          })
-        })
-      default:
-        return state
-  }
-}
-
-function visibiltyFilter(state = SHOW_ALL, action){
-  switch (action.type){
+function visibilityFilter(state = SHOW_ALL, action) {
+  switch (action.type) {
     case SET_VISIBILITY_FILTER:
       return action.filter
     default:
@@ -44,7 +11,47 @@ function visibiltyFilter(state = SHOW_ALL, action){
   }
 }
 
+const todo = (state = {}, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state
+      }
+
+      return Object.assign({}, state, {
+        completed: !state.completed
+      })
+
+    default:
+      return state
+  }
+}
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action)
+      ]
+    case 'TOGGLE_TODO':
+      return state.map(t =>
+        todo(t, action)
+      )
+    default:
+      return state
+  }
+}
+
 const todoApp = combineReducers({
-  visibiltyFilter,
+  visibilityFilter,
   todos
 })
+
+export default todoApp
